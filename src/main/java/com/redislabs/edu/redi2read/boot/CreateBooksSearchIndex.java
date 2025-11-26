517389a02ae0c67bdc4b5a123e3783f01ebe58a4
ichwansh03 <ichwansholihin70@gmail.com>
2025-11-26 20:32:15 +0700

    searching with redis query

---
+++ src/main/java/com/redislabs/edu/redi2read/boot/CreateBooksSearchIndex.java
@@ -0,0 +1,59 @@
+package com.redislabs.edu.redi2read.boot;
+
+import com.redislabs.edu.redi2read.models.Book;
+import com.redislabs.lettusearch.CreateOptions;
+import com.redislabs.lettusearch.Field;
+import com.redislabs.lettusearch.RediSearchCommands;
+import com.redislabs.lettusearch.StatefulRediSearchConnection;
+import io.lettuce.core.RedisCommandExecutionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(6)
+@Slf4j
+public class CreateBooksSearchIndex implements CommandLineRunner {
+
+    @Autowired
+    private StatefulRediSearchConnection<String, String> searchConnection;
+
+    @Value("${app.booksSearchIndexName}")
+    private String searchIndexName;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void run(String... args) throws Exception {
+        RediSearchCommands<String, String> commands = searchConnection.sync();
+        try {
+            commands.ftInfo(searchIndexName);
+        } catch (RedisCommandExecutionException rcee) {
+            if (rcee.getMessage().equals("Unknown Index name")) {
+                CreateOptions<String, String> options = CreateOptions.<String, String>builder()
+                        .prefix(String.format("%s:", Book.class.getName()))
+                        .build();
+
+                Field<String> title = Field.text("title").sortable(true).build();
+                Field<String> subtitle = Field.text("subtitle").build();
+                Field<String> description = Field.text("description").build();
+                Field<String> author0 = Field.tag("authors.[0]").build();
+                Field<String> author1 = Field.tag("authors.[1]").build();
+                Field<String> author2 = Field.tag("authors.[2]").build();
+                Field<String> author3 = Field.tag("authors.[3]").build();
+                Field<String> author4 = Field.tag("authors.[4]").build();
+                Field<String> author5 = Field.tag("authors.[5]").build();
+                Field<String> author6 = Field.tag("authors.[6]").build();
+
+                commands.create(searchIndexName, options,
+                        title, subtitle, description,
+                        author0, author1, author2,
+                        author3, author4, author5,
+                        author6);
+                log.info(">>> Created RediSearch index: {}", searchIndexName);
+            }
+        }
+    }
+}
--- src/main/java/com/redislabs/edu/redi2read/controllers/BookController.java
+++ src/main/java/com/redislabs/edu/redi2read/controllers/BookController.java
@@ -3,7 +3,9 @@ package com.redislabs.edu.redi2read.controllers;
 import com.redislabs.edu.redi2read.models.Book;
 import com.redislabs.edu.redi2read.repositories.BookRepository;
 import com.redislabs.edu.redi2read.repositories.CategoryRepository;
+import com.redislabs.lettusearch.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +28,28 @@ public class BookController {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Value("${app.booksSearchIndexName}")
+    private String searchIndexName;
+
+    @Value("${app.autoCompleteKey}")
+    private String autoCompleteKey;
+
+    @Autowired
+    private StatefulRediSearchConnection<String, String> searchConnection;
+
+    @GetMapping("/authors")
+    public List<Suggestion<String>> authorAutoComplete(@RequestParam("q") String query) {
+        RediSearchCommands<String, String> commands = searchConnection.sync();
+        SuggetOptions options = SuggetOptions.builder().max(20L).build();
+        return commands.sugget(autoCompleteKey, query, options);
+    }
+
+    @GetMapping("/search")
+    public SearchResults<String, String> search(@RequestParam("q")String query) {
+        RediSearchCommands<String, String> commands = searchConnection.sync();
+        return commands.search(searchIndexName, query);
+    }
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> all(
