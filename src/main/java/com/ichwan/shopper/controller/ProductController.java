8430aeeddbd25ee342272a527545fc2b30caf1b7
ichwansh03 <ichwansholihin70@gmail.com>
2025-12-11 21:08:12 +0700

    implement spring cache redis

--- src/main/java/com/ichwan/shopper/controller/ProductController.java
+++ src/main/java/com/ichwan/shopper/controller/ProductController.java
@@ -1,12 +1,48 @@
 package com.ichwan.shopper.controller;
 
+import com.ichwan.shopper.dto.ProductDto;
+import com.ichwan.shopper.entity.Product;
+import com.ichwan.shopper.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService service;
 
+    @GetMapping
+    public ResponseEntity<List<Product>> list(){
+        List<Product> list = service.list();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> get(@PathVariable Long id) {
+        Product product = service.get(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody ProductDto productDto) {
+        Product product = service.create(productDto);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        Product update = service.update(id, productDto);
+        return ResponseEntity.ok(update);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/ichwan/shopper/service/ProductService.java
+++ src/main/java/com/ichwan/shopper/service/ProductService.java
@@ -1,9 +1,16 @@
 package com.ichwan.shopper.service;
 
+import com.ichwan.shopper.dto.ProductDto;
+import com.ichwan.shopper.entity.Product;
 import com.ichwan.shopper.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,4 +19,42 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Cacheable(value = "products", key = "#id")
+    public Product get(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    @CacheEvict(value = "products", allEntries = true)
+    public Product create(ProductDto dto) {
+        Product product = new Product();
+        product.setDescription(dto.getDescription());
+        product.setName(dto.getName());
+        product.setSku(dto.getSku());
+        product.setStock(dto.getStock());
+        product.setPrice(dto.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    @CachePut(value = "products", key = "#id")
+    public Product update(Long id, ProductDto newData) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+        product.setId(id);
+        product.setName(newData.getName());
+        product.setSku(newData.getSku());
+        product.setDescription(newData.getDescription());
+        product.setPrice(newData.getPrice());
+        product.setStock(newData.getStock());
+
+        return productRepository.save(product);
+    }
+
+    @CacheEvict(value = "products", key = "#id")
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> list() {
+        return productRepository.findAll();
+    }
 }
