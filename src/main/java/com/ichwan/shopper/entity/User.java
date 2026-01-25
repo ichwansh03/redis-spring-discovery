4cfebd8d85312456df83a8ef958a07c2f9a474c5
ichwansh03 <ichwansholihin70@gmail.com>
2026-01-25 13:13:44 +0700

    add serializable implement

--- src/main/java/com/ichwan/shopper/entity/User.java
+++ src/main/java/com/ichwan/shopper/entity/User.java
@@ -1,20 +1,26 @@
 package com.ichwan.shopper.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
-@Table(name = "products")
+@Table(name = "users")
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User {
+public class User implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String userId;
     private String username;
     private String email;
